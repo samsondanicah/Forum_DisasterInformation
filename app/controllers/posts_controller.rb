1fe441f68646d0bfa3e2bb13cfe76ed64a7cd6cd
samsondanicah <danicahsamson26@gmail.com>
2023-07-21 11:23:47 +0800

    add permit address

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -47,6 +47,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids: [])
+    params.require(:post).permit(:title, :content, :address, category_ids: [])
   end
 end
