efdaa3e41de1ebb7672c235776642c9e16cd992d
samsondanicah <danicahsamson26@gmail.com>
2023-07-22 18:57:23 +0800

    adding city and barangay. added "ed" in select

--- app/javascript/controllers/location_controller.js
+++ app/javascript/controllers/location_controller.js
@@ -1,15 +1,54 @@
 import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = ['selectedRegionId', 'selectProvinceId']
+    static targets = ['selectedRegionId', 'selectedProvinceId', 'selectedCityId', 'selectedBarangayId' ]
 
     fetchProvinces() {
-        let target = this.selectProvinceIdTarget
+        let target = this.selectedProvinceIdTarget
         $(target).empty();
 
         $.ajax({
             type: 'GET',
             url: '/api/v1/regions/' + this.selectedRegionIdTarget.value + '/provinces',
+            dataType: 'json',
+            success: (response) => {
+                console.log(response)
+                $.each(response, function (index, record) {
+                    let option = document.createElement('option')
+                    option.value = record.id
+                    option.text = record.name
+                    target.appendChild(option)
+                })
+            }
+        })
+    }
+
+    fetchCities() {
+        let target = this.selectedCityIdTarget
+        console.log(target)
+        target.innerHTML = ''
+        $.ajax({
+            type: 'GET',
+            url: '/api/v1/provinces/' + this.selectedProvinceIdTarget.value + '/cities',
+            dataType: 'json',
+            success: (response) => {
+                console.log(response)
+                $.each(response, function (index, record) {
+                    let option = document.createElement('option')
+                    option.value = record.id
+                    option.text = record.name
+                    target.appendChild(option)
+                })
+            }
+        })
+    }
+
+    fetchBarangays() {
+        let target = this.selectedBarangayIdTarget
+        target.innerHTML = ''
+        $.ajax({
+            type: 'GET',
+            url: '/api/v1/cities/' + this.selectedCityIdTarget.value + '/barangays',
             dataType: 'json',
             success: (response) => {
                 console.log(response)
