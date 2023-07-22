0c09bdcb08cab69734d1866b6e13cf08eaa97c18
samsondanicah <danicahsamson26@gmail.com>
2023-07-22 18:59:53 +0800

    no message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_21_093152) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_22_093703) do
   create_table "address_barangays", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "city_id"
     t.string "code"
@@ -78,6 +78,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_21_093152) do
     t.bigint "user_id"
     t.bigint "address_region_id"
     t.bigint "address_province_id"
+    t.bigint "address_city_id"
+    t.bigint "address_barangay_id"
+    t.index ["address_barangay_id"], name: "index_posts_on_address_barangay_id"
+    t.index ["address_city_id"], name: "index_posts_on_address_city_id"
     t.index ["address_province_id"], name: "index_posts_on_address_province_id"
     t.index ["address_region_id"], name: "index_posts_on_address_region_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
