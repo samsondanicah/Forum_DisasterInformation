6f4e56d901f1a806187c5aa3b15ccd03c03f2b71
samsondanicah <danicahsamson26@gmail.com>
2023-07-24 14:47:03 +0800

    adding city and barangay to def index. and typo

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @posts = Post.includes(:categories, :user, :region, :province).page(params[:page]).per(5)
+    @posts = Post.includes(:categories, :user, :region, :province, :city, :barangay).page(params[:page]).per(5)
   end
 
   def new
@@ -13,6 +13,11 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.user = current_user
+    if Rails.env.development?
+      @post.ip_address = Net::HTTP.get(URI.parse('http://checkip.amazonaws.com/')).squish
+    else
+      @post.ip_address = request.remote_ip
+    end
     if @post.save
       flash[:notice] = 'Post created successfully'
       redirect_to posts_path
@@ -53,6 +58,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:title, :content, :address, :region_id, :province_id, :city_id, :barangay_id, category_ids: [])
+    params.require(:post).permit(:title, :content, :address, :address_region_id, :address_province_id, :address_city_id, :address_barangay_id, category_ids: [])
   end
 end
